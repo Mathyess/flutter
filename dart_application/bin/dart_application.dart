a6cdeaed6620ad6dceafeed5c03fff2d53b1e0fe
Matheus Alende <165348908+Mathyess@users.noreply.github.com>
2025-08-12 21:21:08 -0300

    Iniciando em Dart

---
+++ dart_application/bin/dart_application.dart
@@ -0,0 +1,25 @@
+void main() {
+  print("Hello World");
+
+  String rematch = 'Odeio esse jogo!';
+  print(rematch);
+
+  int gol = 2;
+  print(gol);
+
+  double defesas = 0;
+  print(defesas);
+
+  num chutes = 4;
+  print(chutes);
+
+  bool derrota = false;
+  bool vitoria = true;
+  print("O valor verdadeiro é: $vitoria e o falso e $derrota");
+
+  List<String> skins = ['Azul'];
+  print(skins);
+
+  Map pontuacao = {'Moose': 4500, 'Dedamarco': 4200, 'M a t h y e s': 12000};
+  print(pontuacao);
+}
